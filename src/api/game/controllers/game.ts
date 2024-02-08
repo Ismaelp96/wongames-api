db9a805eed2cdf48762fb11918c3878f6c71b4fb
ismaelp96 <ismael6991@live.com>
2024-02-08 14:04:17 -0300

    Instalando JSDOM

---
+++ src/api/game/controllers/game.ts
@@ -0,0 +1,16 @@
+/**
+ * game controller
+ */
+
+import { factories } from "@strapi/strapi";
+
+export default factories.createCoreController(
+  "api::game.game",
+  ({ strapi }) => ({
+    async populate(ctx) {
+      console.log("Rodando no servidor");
+      await strapi.service("api::game.game").populate(ctx.query);
+      ctx.send("Finalizando no client");
+    },
+  })
+);
