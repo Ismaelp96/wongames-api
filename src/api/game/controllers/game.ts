04b629eac3291dee0f1868fd6ba3680650af1919
ismaelp96 <ismael6991@live.com>
2024-02-19 17:20:37 -0300

    Fazendo tratamento de erros

--- src/api/game/controllers/game.ts
+++ src/api/game/controllers/game.ts
@@ -8,9 +8,14 @@ export default factories.createCoreController(
   "api::game.game",
   ({ strapi }) => ({
     async populate(ctx) {
-      console.log("Rodando no servidor");
-      await strapi.service("api::game.game").populate(ctx.query);
-      ctx.send("Finalizando no client");
+      const options = {
+        limit: 48,
+        order: "desc:trending",
+        ...ctx.query,
+      };
+
+      await strapi.service("api::game.game").populate(options);
+      ctx.send("Finished populating games!");
     },
   })
 );
